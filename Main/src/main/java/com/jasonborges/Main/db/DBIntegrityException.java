fa6f0d803c25a9e2e0ebbaf3fc7abbcaa6eb054e
jason <jgbscic@gmail.com>
2023-06-19 18:51:45 -0300

    Project created

---
+++ Main/src/main/java/com/jasonborges/Main/db/DBIntegrityException.java
@@ -0,0 +1,10 @@
+package com.jasonborges.Main.db;
+
+public class DBIntegrityException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public DBIntegrityException(String msg) {
+        super(msg);
+    }
+}
